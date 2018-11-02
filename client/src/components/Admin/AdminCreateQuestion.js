fbeb6a3ae75bcb88e8579d9f75ecb0ccc978ad38
Bianca Jaquez Chavez <biancajaquezchavez@Bianca-Jaquezs-MacBook-Pro.local>
2018-11-02 14:44:36 -0700

    admin stuff

--- client/src/components/Admin/AdminCreateQuestion.js
+++ client/src/components/Admin/AdminCreateQuestion.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Axios from 'axios';
 import Popup from 'reactjs-popup';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 
@@ -21,6 +21,7 @@ class Admin extends React.Component {
 
     }
 
+    
     componentDidMount() {
         this.getCategories();
         console.log(this.state.codingCategories);
@@ -33,15 +34,15 @@ class Admin extends React.Component {
 
             var inputs = []
             var expected = []
-            
-            for(var i=0; i<event.target.elements.length; i++){
-                if(event.target.elements[i].id === "input"){
+
+            for (var i = 0; i < event.target.elements.length; i++) {
+                if (event.target.elements[i].id === "input") {
                     inputs.push(event.target.elements[i].value)
                 }
-                else if(event.target.elements[i].id === "expected"){
+                else if (event.target.elements[i].id === "expected") {
                     expected.push(event.target.elements[i].value)
                 }
-                
+
             }
             console.log(event.target.elements)
             console.log(inputs)
@@ -69,12 +70,13 @@ class Admin extends React.Component {
 
             console.log(parsedInputs)
             console.log(parsedExpected)
-        
+            this.handleSubmitCoding(parsedInputs, parsedExpected);
+
         }
-        else if (this.state.questionType){
+        else if (this.state.questionType) {
             this.handleSubmitInterview()
         }
-        
+
     }
     handleClick = (event) => {
         this.setState({ [event.target.name]: event.target.value });
@@ -86,7 +88,7 @@ class Admin extends React.Component {
     }
 
     handleSubmitCoding = (parsedInputs, parsedExpected) => {
-        
+
 
         var questionObject = {
             text: this.state.questionText,
@@ -109,10 +111,10 @@ class Admin extends React.Component {
 
         Axios.post("/api/createQuestion", questionObject).then((response) => {
             console.log(response)
-            this.setState({postQuestionResponse: "success"});
+            this.setState({ postQuestionResponse: "success" });
         }).catch((err) => {
             console.log(err)
-            this.setState({postQuestionResponse: "fail"});
+            this.setState({ postQuestionResponse: "fail" });
         })
     };
 
@@ -129,7 +131,7 @@ class Admin extends React.Component {
         }
         Axios.post("/api/createQuestion", questionObject).then((response) => {
             console.log(response)
-            this.setState({postQuestionResponse: "success"});
+            this.setState({ postQuestionResponse: "success" });
         });
     }
     renderQuestionOptions = () => {
@@ -137,96 +139,96 @@ class Admin extends React.Component {
         if (this.state.questionType === "interview") {
 
             return (
-                <div class="form-group">
+                <div className="form-group">
                     <label for="question-answer">Question Answer</label>
                     <input
-                        class="form-control"
+                        className="form-control"
                         id="question-answer"
                         name="questionAnswer"
                         onChange={this.handleChange}
-                        value={this.state.questionAnswer} 
-                        required/>
+                        value={this.state.questionAnswer}
+                        required />
                     <Popup
-                        trigger={<button className="button"> Submit </button>}
+                        trigger={<button className="btn btn-warning button" style = {{marginTop: "15px"}}> Submit </button>}
                         modal
                         closeOnDocumentClick>
-                        <div> 
+                        <div>
                             {this.modalContent()}
                         </div>
-                        </Popup>
+                    </Popup>
                 </div>
             )
         } else if (this.state.questionType === "coding") {
 
             return (
-                <div class="form-row">
-                    <label for="test1" class="col-sm-2 col-form-label">Test 1</label>
-                    <div class="form-group col-md-5">
+                <div className="form-row">
+                    <label for="test1" className="col-sm-2 col-form-label">Test 1</label>
+                    <div className="form-group col-md-5">
                         <label for="input">Input</label>
                         <input
                             type="text"
-                            class="form-control"
+                            className="form-control"
                             id="input"
-                            name="input1Par1" 
-                            required/>
+                            name="input1Par1"
+                            required />
                     </div>
-                    <div class="form-group col-md-5">
+                    <div className="form-group col-md-5">
                         <label for="expected">Expected Output</label>
                         <input type="text"
-                            class="form-control"
+                            className="form-control"
                             id="expected"
                             name="expected1"
-                            required/>
+                            required />
                     </div>
-                    <label for="test1" class="col-sm-2 col-form-label">Test 2</label>
-                    <div class="form-group col-md-5">
+                    <label for="test1" className="col-sm-2 col-form-label">Test 2</label>
+                    <div className="form-group col-md-5">
                         <label for="input">Input</label>
                         <input
                             type="text"
-                            class="form-control"
+                            className="form-control"
                             id="input"
                             name="input2Par1"
                             required
                         />
                     </div>
-                    <div class="form-group col-md-5">
+                    <div className="form-group col-md-5">
                         <label for="expected">Expected Output</label>
                         <input type="text"
-                            class="form-control"
+                            className="form-control"
                             id="expected"
                             name="expected2"
                             required
                         />
                     </div>
-                    <label for="test1" class="col-sm-2 col-form-label">Test 3</label>
-                    <div class="form-group col-md-5">
+                    <label for="test1" className="col-sm-2 col-form-label">Test 3</label>
+                    <div className="form-group col-md-5">
                         <label for="input">Input</label>
                         <input
                             type="text"
-                            class="form-control"
+                            className="form-control"
                             id="input"
                             name="input3Par1"
                             required
                         />
                     </div>
-                    <div class="form-group col-md-5">
+                    <div className="form-group col-md-5">
                         <label for="expected">Expected Output</label>
                         <input
                             type="text"
-                            class="form-control"
+                            className="form-control"
                             id="expected"
                             name="expected3"
                             required
                         />
                     </div>
                     <Popup
-                        trigger={<button className="button"> Submit</button>}
+                        trigger={<button className="btn btn-warning button" style = {{marginTop: "15px"}}> Submit</button>}
                         modal
                         closeOnDocumentClick>
-                        <div> 
+                        <div>
                             {this.modalContent()}
                         </div>
-                        </Popup>
+                    </Popup>
                 </div>
 
             )
@@ -243,7 +245,6 @@ class Admin extends React.Component {
                     if (codingCategories.indexOf(item.category) === -1) {
                         codingCategories.push(item.category);
                     }
-
                 } else {
                     if (interviewCategories.indexOf(item.category) === -1) {
                         interviewCategories.push(item.category);
@@ -263,30 +264,26 @@ class Admin extends React.Component {
         console.log(this.state.codingCategories)
         console.log(this.state.interviewCategories)
         console.log(this.state.questionType)
-        if (this.state.questionType === "coding") {
-            return (
-                <div>
-                    <p>Current Categories</p>
-                    {this.state.codingCategories.map(element => (
 
-                        <button
-                            onClick={this.handleCategoryClick}
-                        >{element}</button>
-                    ))}
-                </div>
-            )
-        } if (this.state.questionType === "interview") {
-            return (
-                <div>
-                    <p>Current Categories</p>
-                    {this.state.interviewCategories.map(element => (
-                        <button
-                            onClick={this.handleCategoryClick}
-                        >{element}</button>
-                    ))}
-                </div>
-            )
+        var categories = []
+
+        if (this.state.questionType === "coding"){
+            categories = this.state.codingCategories;
+        }else if(this.state.questionType === "interview"){
+            categories = this.state.interviewCategories;
         }
+        return (
+            <div>
+            <p style = {{fontStyle: "italic", marginTop: "5px", marginBottom: "5px"}}>Current Categories</p>
+            {categories.map(element => (
+                <button className = "btn-sm btn-info" style ={{margin: "2px"}}
+                    onClick={this.handleCategoryClick}
+                >{element}</button>
+                
+            ))}
+        </div>
+
+        )
     }
 
     handleCategoryClick = (event) => {
@@ -296,15 +293,15 @@ class Admin extends React.Component {
     }
 
     modalContent = () => {
-        if(this.state.postQuestionResponse === "success"){
+        if (this.state.postQuestionResponse === "success") {
             return (
                 <div>
                     <h2>Your question has been created!</h2>
                     <button>Back Home</button>
-                    <Link to="/AdminCreateQuestion"><button>Create another question</button></Link>
+                    <button onClick={ this.refreshPage }>Create another question</button>
                 </div>
             )
-        }else if(this.state.postQuestionResponse === "fail"){
+        } else if (this.state.postQuestionResponse === "fail") {
             return (
                 <div>
                     <h4>There was an error with your post</h4>
@@ -313,68 +310,73 @@ class Admin extends React.Component {
         }
     }
 
+    refreshPage = () =>{ 
+        window.location.reload(); 
+    }
+
     render() {
         return (
-            <div className="row" >
-                <h1>Create a New Question</h1>
+            <div className="row" style ={{width: "50%", marginLeft: "auto", marginRight: "auto", marginTop: "60px"}} >
+                <h4 style ={{width:'100%',textAlign:"center"}}>Create a New Question</h4>
                 <div className="col-md-12">
-                    <form onSubmit={this.handleSubmit}>
-                        <div class="form-check">
+                    <form style = {{border: "1px solid black", padding: "30px", borderRadius: "5px"}}onSubmit={this.handleSubmit}>
+                    <p style = {{marginBottom: "5px", fontWeight: "bold"}} >Question Type</p>
+                        <div className="form-check">
                             <input
                                 className="form-check-input"
                                 type="radio"
                                 name="questionType"
                                 id="interview"
                                 value="interview"
-                                onClick={this.handleClick} 
-                                required/>
-                            <label class="form-check-label" for="interview">
+                                onClick={this.handleClick}
+                                required />
+                            <label className="form-check-label" for="interview">
                                 Interview
                         </label>
                         </div>
-                        <div class="form-check">
+                        <div className="form-check">
                             <input
                                 className="form-check-input"
                                 type="radio"
                                 name="questionType"
                                 id="coding"
                                 value="coding"
-                                onClick={this.handleClick} 
-                                required/>
-                            <label class="form-check-label" for="coding">
+                                onClick={this.handleClick}
+                                required />
+                            <label className="form-check-label" for="coding">
                                 Coding
                         </label>
                         </div>
 
                         {this.renderCategories()}
 
-                        <div class="form-group">
-                            <label for="category">Category</label>
+                        <div className="form-group">
+                            <label for="category" style = {{fontWeight: "bold"}}>Category</label>
                             <input
-                                class="form-control"
+                                className="form-control"
                                 id="category"
                                 name="category"
                                 onChange={this.handleChange}
-                                value={this.state.category} 
-                                required/>
+                                value={this.state.category}
+                                required />
                         </div>
-                        <div class="form-group">
-                            <label for="questionText">Question Text</label>
+                        <div className="form-group">
+                            <label for="questionText" style = {{fontWeight: "bold"}}>Question Text</label>
                             <input
-                                class="form-control"
+                                className="form-control"
                                 name="questionText"
                                 id="questionText"
                                 onChange={this.handleChange}
-                                value={this.state.questionText} 
-                                required/>
+                                value={this.state.questionText}
+                                required />
                         </div>
 
-                        {this.renderQuestionOptions()}   
+                        {this.renderQuestionOptions()}
 
-                        
+
                     </form>
-                    
-                    
+
+
 
                 </div>
                 <div>
--- client/src/components/Instructions/instructions.js
+++ client/src/components/Instructions/instructions.js
@@ -6,6 +6,7 @@ const Instructions = (props)=>{
     <div> <h4 className='text-center' style = {{width: '100%',borderBottom:'1px solid grey'}}>Instructions</h4></div>
     <p style ={{textAlign:"center"}}>{props.text}</p>
     <br/>
+    <p style ={{textAlign:"center"}}>Write your code inside the given function</p>
     <hr/>
     <br/>
     <p style ={{textAlign:"center"}}><h5>Input: {JSON.stringify(props.tests[0].input)}</h5></p>
