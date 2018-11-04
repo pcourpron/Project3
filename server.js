5bd9b02ccec2b8855d823ae4f3c31a06f58d90c9
pcourpron <pierrecourpron@gmail.com>
2018-11-03 20:44:10 -0700

    add production

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 // Define API routes here
